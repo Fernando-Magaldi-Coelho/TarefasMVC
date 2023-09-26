60f1bfa3783830247c1463e217ce2d3d8578574a
Fernando Magaldi Coelho <135265421+Fernando-Magaldi-Coelho@users.noreply.github.com>
2023-09-25 21:47:17 -0300

    Add files via upload

---
+++ Tarefa/libs/PHPMailer/inc.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . "/src/Expetion.php";
+require __DIR__ . "/src/OAuth.php";
+require __DIR__ . "/src/PHPMailer.php";
+require __DIR__ . "/src/POP3.php";
+require __DIR__ . "/src/SMIP.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
